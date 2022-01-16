a3dc44868ed12d647662ef82608fd1964dcf74d7
AKARSH121 <55624313+AKARSH121@users.noreply.github.com>
2022-01-16 23:01:24 +0530

    Update USerArray.java

--- USerArray.java
+++ USerArray.java
@@ -1,16 +1,43 @@
 package com.AkarshJavaAssignment;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class USerArray {
+			
+			public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		int a[]= {10,20,60,80,100};
-for(int i=0;i<5;i++) {
-System.out.println("Elements of array a: "+a[i]);
-}
-int[][] b = {
-            {2, 4, 6, 8}
-            };
-      
-      System.out.println("\nLength of row 1: " + b[0].length);
-      }
-}
+
+				String[] listOfEmails= {"akash.v12@gmail.com","darshan.123@gmail.com"};
+				String trueExample="runqw.1234@gmail.com",falseExample="raohit.k1234@gmail.com";
+
+				//String trueExample=new Scanner(System.in).nextLine() to take input from user
+
+				boolean a=emailVerification(trueExample, listOfEmails);
+				boolean b=emailVerification(falseExample, listOfEmails);
+
+				output(a);
+				output(b);
+
+
+			}
+		static boolean emailVerification(String email,String[] listOfEmails) {
+			boolean flag=false;
+			Pattern p = Pattern.compile(email);
+			for(int i=0; i<listOfEmails.length; i++) {
+				Matcher m =p.matcher(listOfEmails[i]);
+				if(m.find()) {
+					flag = true;
+				}
+			}
+			return flag;
+		}
+
+		static void output(boolean result) {
+			if(result)
+				System.out.println("Email verification passed");
+			else
+				System.out.println("Email verification failed");
+		}
+			
+	}
+
